b63b345c33e353e60c94eb59545182328e2f3787
yourFriend <yourFriend@email.com>
2022-02-20 12:56:49 +0530

    done yaar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import home from './components/home';
 
-const center = {
-  textAlign: 'center'
-}
-
 function App() {
+<<<<<<< HEAD
   
   return (
     <>
     <home center={center}/>
     </>
   )
+=======
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> parent of 516cd7e (Done buildig the app using useState)
 }
 
 export default App;
